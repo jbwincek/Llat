b93a9de73d07ca5dbc9413a0285310a4aa7f54bb
J B Wincek <jbwincek@gmail.com>
2016-02-16 15:47:08 -0500

    Speech_recognition_POC.py now does background recognition
    ADJUSTED: instead of doing a single round of foreground recognition, it
    now implements listen_in_background().

--- llat/speech_recognition_POC.py
+++ llat/speech_recognition_POC.py
@@ -1,6 +1,7 @@
 import urwid
 import random
 import speech_recognition as sr
+import time
 
 ACCESS_TOKEN = '6W3RVPVZMJIK3JQ3VPC27D7ALF6HVNBI'
 
@@ -9,14 +10,37 @@ Speech recognition proof of concept, utilizing the speech_recognition package
 and wit.ai
 """
 
+
+def callback(recognizer,audio):
+    try:
+        print("heard: {}".format(recognizer.recognize_wit(audio, ACCESS_TOKEN)))
+    except sr.UnknownValueError:
+        print('unknown value')
+    except sr.RequestError as e:
+        print("Could not request results from wit Speech Recognition service; {0}".format(e))
+
+
 r = sr.Recognizer()
-with sr.Microphone() as source:
-    print('say something')
-    audio = r.listen(source)
+m = sr.Microphone( sample_rate=44100, chunk_size=8192)
+with m as source:
+    r.adjust_for_ambient_noise(source)
 
-response = r.recognize_wit(audio, ACCESS_TOKEN,show_all=True)
 
-print(response)
+stop_listening = r.listen_in_background(m,callback)
+print('say something')
+
+for step in range(500):
+    time.sleep(0.1)
+stop_listening()
+
+
+
+
+
+
+#response = r.recognize_wit(audio, ACCESS_TOKEN,show_all=True)
+
+#print(response)
 
 
 
