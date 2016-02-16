979d9d965f9d75bb45497467422b63f784d99162
J B Wincek <jbwincek@gmail.com>
2016-02-16 13:50:12 -0500

    removed PyWit dependency
    REMOVED: SpeechRecognition can natively do wit.ai integration, this is
    now being used instead of PyWit

--- llat/speech_recognition_POC.py
+++ llat/speech_recognition_POC.py
@@ -1,22 +1,20 @@
 import urwid
 import random
 import speech_recognition as sr
-from wit import Wit
 
 ACCESS_TOKEN = '6W3RVPVZMJIK3JQ3VPC27D7ALF6HVNBI'
 
 """
 Speech recognition proof of concept, utilizing the speech_recognition package
-and wit.ai  
+and wit.ai
 """
 
-w = Wit(ACCESS_TOKEN)
 r = sr.Recognizer()
 with sr.Microphone() as source:
     print('say something')
     audio = r.listen(source)
 
-response = w.post_speech(audio.get_wav_data())
+response = r.recognize_wit(audio, ACCESS_TOKEN,show_all=True)
 
 print(response)
 
