23cfc66b93affcfb244d0a6ecabc4a1fbbba2505
J B Wincek <jbwincek@Jessies-MBP.local>
2016-02-22 23:43:16 -0500

    Got box_drawing as a coroutine style generator to work (mostly)
    See technique 5 in experiment/generator_test.py

--- README.rst
+++ README.rst
@@ -4,8 +4,11 @@ Llat: The Last Layer Algorithm Trainer
 A python tool for learning speedcubing algorithms with voice commands.
 ----------------------------------------------------------------------
 
-Uses a flashcard style system to help you learn last layer algorithms.
+A interactive terminal based app for a flashcard style system to help you learn last layer
+algorithms.
+
 The algorithms are from `ALGDB.net <http://algdb.net/>`__.
 
-Currently very Alpha
+Currently very Alpha, most work going on in the ``experimental/`` subdirectory, figuring out how to
+get the components to work. 
 
--- experimental/generator_test.py
+++ experimental/generator_test.py
@@ -33,12 +33,11 @@ def run_1():
         print(item)
 
 
+
 """ Notes on technique 2: (trying to use send() methods to control flow in a generator)
         * it works
         * can use the generator as a closure
 """
-
-
 def gen_test_2():
     value_1 = 1
     value_2 = 2
@@ -55,9 +54,8 @@ def run_2():
     next(gen)  # don't forget to initialize the generator to get it up to the yield point
     gen.send(5)
     gen.send('hello')
-
-
 #  run_2()
+
 
 """ Notes on technique 3: (trying to use async / await for keyboard input handling)
     * async is hard
@@ -86,8 +84,9 @@ def run_3():
     loop = asyncio.get_event_loop()
     loop.run_until_complete(gen_test_3())
     loop.close()
-
 #  run_3()
+
+
 
 """ Notes on technique 4: box drawing
     * render_to_terminal flushes when it renders
@@ -134,5 +133,55 @@ def run_4():
             win.render_to_terminal(returns_FSArray_boxes(i,i*2))
             time.sleep(.2)
 
-run_4()
+#run_4()
 
+
+
+""" Notes on technique 5: like technique 4, but as a generator
+    * it works, sorta:
+        * sending in (height, width) uses raises a TypeError 'NoneType object is not iterable which
+          seems related to the `if height:` check. Well that's how I got around it when just sending
+          in height, but `if height and width:` doesn't do it. Maybe an or?
+"""
+
+
+
+def yields_FSArray_boxes(height, width, max=15):
+    top_left = '╒'
+    top_right = '╕'
+    bottom_left = '╘'
+    bottom_right = '╛'
+    horizontal = '═'
+    vertical = '│'
+    for i in range(max):
+        if height:
+            box_array = fsarray([' ' * width for _ in range(height)])
+            # FSArrays are height,width <- weird but true
+            box_array[0,0] = top_left
+            box_array[0,width-1] = top_right
+            box_array[height-1,0] = bottom_left
+            box_array[height-1,width-1] = bottom_right
+            if width>2:
+                span = width-2
+                for index in range(span):
+                    box_array[0,index+1] = horizontal
+                    box_array[height-1, index + 1] = horizontal
+            if height>2:
+                span = height-2
+                for index in range(span):
+                    box_array[index+1,0] = vertical
+                    box_array[index+1,width-1] = vertical
+        height = yield box_array
+
+
+def run_5():
+    with FullscreenWindow() as win:
+        i = 1
+        box_generator = yields_FSArray_boxes(2,2, max=40)
+        for box in box_generator:
+            win.render_to_terminal(box)
+            i+=1
+            box_generator.send(i)
+            time.sleep(.2)
+
+run_5()
